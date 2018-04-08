0146fab8e16c9a9278cd1eca4729fa750a40deb5
Olivier <Olivier@OLIVIER-PC>
2018-04-08 19:29:06 +0200

    Added forgotten new files

---
+++ app/src/main/java/fr/esgi/alloeatsclientapp/activities/CreateAccountActivity.kt
@@ -0,0 +1,71 @@
+package fr.esgi.alloeatsclientapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.inaka.killertask.KillerTask
+import fr.esgi.alloeatsclientapp.R
+import fr.esgi.alloeatsclientapp.api.requests.APIUser
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    private var apiUser : APIUser? = APIUser()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        val inputFields = listOf<EditText>(
+                findViewById(R.id.emailInput), findViewById(R.id.passwordInput),
+                findViewById(R.id.phoneNumberInput),
+                findViewById(R.id.countryInput), findViewById(R.id.cityInput),
+                findViewById(R.id.addressInput), findViewById(R.id.zipCodeInput),
+                findViewById(R.id.firstNameInput), findViewById(R.id.lastNameInput)
+        )
+
+        val abortButton: Button? = findViewById(R.id.abortButton)
+        val registerButton: Button? = findViewById(R.id.registerButton)
+
+        registerButton?.setOnClickListener({
+            for (inputField: EditText? in inputFields){
+                if(inputField == null || inputField.length() <= 0){
+                    Toast.makeText(this, "All fields must be filled.",
+                            Toast.LENGTH_SHORT).show()
+                } else {
+                    KillerTask(
+                        {
+                            apiUser?.createAccount(this.applicationContext,
+                                    toEditTextStrings(inputFields))
+                        },
+                        {
+                            Toast.makeText(this, "Your account has been created!",
+                                    Toast.LENGTH_SHORT).show()
+                            Log.i("CreateAccountActivity", "Account created.")
+                        }).go()
+                }
+            }
+        })
+
+        abortButton?.setOnClickListener({
+
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+
+    private fun toEditTextStrings(inputFields: List<EditText>?): List<String>{
+        val inputFieldsTexts: MutableList<String>? = null
+
+        for (inputField: EditText in inputFields!!){
+            inputFieldsTexts?.add(inputField.text.toString())
+        }
+
+        return inputFieldsTexts?.toList()!!
+    }
+}
